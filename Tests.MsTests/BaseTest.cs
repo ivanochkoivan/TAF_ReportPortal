5b62cc91fd679979108db04c02295e850f914fcc
Ivan <ivanochkoivan2000@gmail.com>
2024-04-02 16:38:36 +0200

    add additional runner

---
+++ Tests.MsTests/BaseTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using TAF_ReportPortal_Configuration;
+
+namespace TAF_ReportPortal_Tests.MsTests
+{
+    public class BaseTest
+    {
+        protected Logger Logger { get; private set; }
+        protected IWebDriver WebDriver { get; private set; }
+        protected HttpClient HttpClient { get; private set; }
+
+        public void InitiateLogger()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(builder => builder.AddConsole())
+                .BuildServiceProvider();
+
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+
+            Logger = new Logger(factory.CreateLogger<Logger>());
+        }
+
+        [TestInitialize]
+        public void BaseSetUp()
+        {
+            WebDriver = TestEnvironment.Instance.WebDriver;
+            HttpClient = TestEnvironment.Instance.HttpClient;
+
+            InitiateLogger();
+        }
+        [TestCleanup]
+        public void BaseTearDown()
+        {
+            TestEnvironment.Instance.After();
+        }
+    }
+}
---
+++ Tests.MsTests/LoginTest.cs
@@ -0,0 +1,20 @@
+﻿using TAF_ReportPortal_Business;
+using Microsoft.Extensions.Logging;
+using TAF_ReportPortal_Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TAF_ReportPortal_Tests.MsTests
+{
+    [TestClass]
+    public class LoginTest : BaseTest
+    {
+        [TestMethod] 
+        public void Login()
+        {
+            Logger.Log("Initiate driver");
+            LoginPage loginPage = new LoginPage(WebDriver);
+            loginPage.GoToLoginPage();
+            loginPage.LoginWithData(TestEnvironment.Instance.Config.UiTestConfig.Login, TestEnvironment.Instance.Config.UiTestConfig.Password);
+        }
+    }
+}
