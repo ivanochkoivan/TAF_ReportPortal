6fcabfead49456a900d839754858aa421e1d04ac
Ivan <ivanochkoivan2000@gmail.com>
2024-04-22 13:27:50 +0300

    fix all warnings

--- Business/Menu.cs
+++ Business/Menu.cs
@@ -1,22 +1,20 @@
 ﻿using OpenQA.Selenium;
-using SeleniumExtras.PageObjects;
 
 namespace TAF_ReportPortal_Business
 {
     public class Menu : BasePage
-    { 
-        [FindsBy(How = How.XPath, Using = "//div[contains(@class, \"sidebar__top-block\")]/div[1]")]
-        private IWebElement DashboardButton;
+    {
+        private IWebElement DashboardButton => driver.FindElement(By.XPath("//div[contains(@class, \"sidebar__top-block\")]/div[1]"));
 
         private string DashboardButtonXpath = "//div[contains(@class, \"sidebar__top-block\")]/div[1]";
 
         public Menu(IWebDriver driver) : base(driver)
         {
             WaitingWhilePageIsLoading(DashboardButtonXpath);
-            PageFactory.InitElements(driver, this);
         }
 
-        public void GoToDashboardPage() { 
+        public void GoToDashboardPage()
+        {
             DashboardButton.Click();
         }
     }
